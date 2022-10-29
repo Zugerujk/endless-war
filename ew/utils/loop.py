90420a0261b76da6066df8253fdbded7a40fed54
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2022-10-29 06:07:17 -0500

    Fixed day_map, nat. disast. rebalan wonderworld

--- ew/utils/loop.py
+++ ew/utils/loop.py
@@ -1381,7 +1381,7 @@ async def clock_tick_loop(id_server = None, force_active = False):
                             await apt_utils.rent_time(id_server)
                             ewutils.logMsg("...finished rent calc.")
 
-                        if random.randint(1, 11) == 1: # 1/11 chance to start a random poi event
+                        if random.randint(1, 9) == 1: # 1/9 chance to start a random poi event
                             ewutils.logMsg("Creating POI event...")
                             await weather_utils.create_poi_event(id_server)
 
