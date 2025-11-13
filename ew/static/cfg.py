984d08392cb3a9c1868731d3f89ca6f24c9f668e
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2025-11-13 05:59:07 -0600

    Welcome Back Double Halloween?

--- ew/static/cfg.py
+++ ew/static/cfg.py
@@ -3,7 +3,7 @@ import random
 # Global configuration options.
 
 
-version = "v4.236.3 SEASON 4 ACT 2 - No Double No Hallow"
+version = "v4.236.4 SEASON 4 ACT 2 - Triple Halloween"
 
 
 
@@ -28,9 +28,9 @@ permissions_tries = 1
 territory_time_gain = 10
 
 #Double Halloween Features
-dh_active = False
+dh_active = True
 #Existing Stages for Double Halloween. As the years go by we may add on to this
-dh_stage = 0
+dh_stage = 11
 
 #Slimernalia Features
 slimernalia_active = False
