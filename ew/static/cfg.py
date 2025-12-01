f94501eadfddd4f0b2b14dcb42a1b34f7437a425
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2025-12-01 08:09:48 -0600

    I am like so stumped

--- ew/static/cfg.py
+++ ew/static/cfg.py
@@ -28,9 +28,9 @@ permissions_tries = 1
 territory_time_gain = 10
 
 #Double Halloween Features
-dh_active = True
+dh_active = False
 #Existing Stages for Double Halloween. As the years go by we may add on to this
-dh_stage = 11
+dh_stage = 3
 
 #Slimernalia Features
 slimernalia_active = False
