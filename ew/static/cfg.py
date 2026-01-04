6a899fb8e303b0fd13e33929e9d2049a35e59923
Zugerujk <84615565+Zugerujk@users.noreply.github.com>
2026-01-03 20:34:32 -0600

    no more slimernalia

--- ew/static/cfg.py
+++ ew/static/cfg.py
@@ -3,7 +3,7 @@ import random
 # Global configuration options.
 
 
-version = "v4.237.1 SEASON 4 ACT 2 - Slimernalia 2025"
+version = "v4.238 SEASON 4 ACT 2 - No more !yo."
 
 
 
@@ -33,7 +33,7 @@ dh_active = False
 dh_stage = 0
 
 #Slimernalia Features
-slimernalia_active = True
+slimernalia_active = False
 
 #Swilldermuk Features
 swilldermuk_active = False
